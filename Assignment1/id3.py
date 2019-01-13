18376948170d3ce4f19f25b6485d0fdf5764a667
Dhruv Kapoor <admin@Dhruvs-Air.hitronhub.home>
2019-01-13 10:55:04 -0800

    added data loaders

--- Assignment1/id3.py
+++ Assignment1/id3.py
@@ -2,23 +2,16 @@ import numpy
 import sklearn
 import matplotlib
 import pandas
+from utilities.data_fetcher import get_mammography_data
+from utilities.data_fetcher import get_happiness_data
 
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeClassifier
 
-# MAMMOGRAPHY
-# 162 missing values
-# 130 lines removed
-# ages transformed from years to decades
+train_attributes, train_labels, test_attributes, test_labels = get_happiness_data()
 
-# HAPPINESS
-# moved label from first to last in columns
-
-#data = pandas.read_csv('/Users/admin/Downloads/happiness_cleaned.csv', ',', encoding = 'utf-16')
-data = pandas.read_csv('/Users/admin/Downloads/mammography_cleaned.csv', ',', encoding = 'utf-8')
-attribute_cols = ['BI_RADS', 'age', 'shape', 'margin', 'density']
-attributes = data.loc[:, attribute_cols]
-labels = data.label
+print(train_labels.shape)
+print(test_labels.shape)
 
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(attributes, labels)
+classifier.fit(train_attributes, train_labels)
---
+++ Assignment1/utilities/data_fetcher.py
@@ -0,0 +1,37 @@
+import pandas
+
+# use 60:20:20
+
+# 162 missing values
+# 130 lines removed
+# ages transformed from years to decades
+def get_mammography_data():
+    data = pandas.read_csv('utilities/data/mammography_data.csv', ',', encoding = 'utf-8')
+
+    attribute_cols = ['BI_RADS', 'age', 'shape', 'margin', 'density']
+    label_col = ['diagnosis']
+    train_count = 664
+
+    train_attributes = data.loc[:train_count - 1, attribute_cols]
+    train_labels = data.loc[:train_count - 1, label_col]
+
+    test_attributes = data.loc[train_count:, attribute_cols]
+    test_labels = data.loc[train_count:, attribute_cols]
+
+    return train_attributes, train_labels, test_attributes, test_labels
+
+# moved label from first to last in columns
+def get_happiness_data():
+    data = pandas.read_csv('utilities/data/happiness_data.csv', ',', encoding = 'utf-16')
+
+    attribute_cols = ['city_info','housing_cost','school_quality','police_trust','street_maintenance','community_events']
+    label_col = ['happy']
+    train_count = 114
+
+    train_attributes = data.loc[:train_count - 1, attribute_cols]
+    train_labels = data.loc[:train_count - 1, label_col]
+
+    test_attributes = data.loc[train_count:, attribute_cols]
+    test_labels = data.loc[train_count:, attribute_cols]
+
+    return train_attributes, train_labels, test_attributes, test_labels
