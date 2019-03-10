ef934c7cdf638ee9a58003251c239bd18d7caef8
Dhruv Kapoor <admin@Dhruvs-Air.hitronhub.home>
2019-03-10 11:12:10 -0700

    finished kmeans

--- Assignment2/k_means.py
+++ Assignment2/k_means.py
@@ -11,6 +11,7 @@ from sklearn.metrics import confusion_matrix,classification_report
 from sklearn.exceptions import DataConversionWarning
 from warnings import filterwarnings
 import warnings
+from mpl_toolkits.mplot3d import Axes3D
 
 def run_mamm():
     train_count, train_attributes, train_labels, test_attributes, test_labels = get_mammography_data(100)
@@ -25,9 +26,15 @@ def run_mamm():
     y.columns == ['diagnosis']
 
     color_theme = np.array(['magenta', 'brown'])
+    plt.title('Actual Mammography Clusters')
+    plt.xlabel('Age')
+    plt.ylabel('BI-RADS Score')
     plt.scatter(x=mamm_df.age, y=mamm_df.BI_RADS, c=color_theme[train_labels.values[:,0]], s=5)
     plt.show()
 
+    plt.title('Learned Mammography Clusters')
+    plt.xlabel('Age')
+    plt.ylabel('BI-RADS Score')
     plt.scatter(x=mamm_df.age, y=mamm_df.BI_RADS, c=color_theme[clustering.labels_], s=5)
     plt.show()
 
@@ -38,17 +45,29 @@ def run_skin():
 
     clustering = KMeans(n_clusters=2, random_state=0)
     clustering.fit(X)
-
+    
     skin_df = pd.DataFrame(train_attributes)
     skin_df.columns = ['blue','green','red']
     y.columns == ['is_non_skin']
     
     color_theme = np.array(['purple', 'brown'])
-    
-    plt.scatter(x=skin_df.green, y=skin_df.red, c=color_theme[train_labels.values[:,0]], s=5)
+
+    fig1 = plt.figure()
+    ax1 = Axes3D(fig1)
+    ax1.set_title("Actual Skin Clusters")
+    ax1.set_xlabel("Blue")
+    ax1.set_ylabel("Green")
+    ax1.set_zlabel("Red")
+    ax1.scatter(xs=skin_df.blue, ys=skin_df.green, zs=skin_df.red, c=color_theme[train_labels.values[:,0]], s=1)
     plt.show()
     
-    plt.scatter(x=skin_df.green, y=skin_df.red, c=color_theme[clustering.labels_], s=5)
+    fig2 = plt.figure()
+    ax2 = Axes3D(fig2)
+    ax2.set_title("Learned Skin Clusters")
+    ax2.set_xlabel("Blue")
+    ax2.set_ylabel("Green")
+    ax2.set_zlabel("Red")
+    ax2.scatter(xs=skin_df.blue, ys=skin_df.green, zs=skin_df.red, c=color_theme[clustering.labels_], s=1)
     plt.show()
 
 warnings.filterwarnings(action='ignore', category=DataConversionWarning)
