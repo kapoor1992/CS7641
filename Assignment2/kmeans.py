8250e7ed93411475f2f43ad4ce03095f553b3d25
Dhruv Kapoor <kapoor_1992@hotmail.com>
2019-03-14 22:56:16 -0700

    redid clustering

---
+++ Assignment2/kmeans.py
@@ -0,0 +1,64 @@
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_samples, silhouette_score
+from utilities.data_fetcher import *
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import numpy as np
+
+def run(dataset):
+    if dataset == 1:
+        train_count, X, y, test_attributes, test_labels = get_mammography_data(100)
+    else:
+        train_count, X, y, test_attributes, test_labels = get_skin_data(10)
+
+    range_n_clusters = [2, 3, 4, 5, 6]
+
+    for n_clusters in range_n_clusters:
+        fig, ax1 = plt.subplots(1, 1)
+
+        ax1.set_xlim([-1, 1])
+        ax1.set_ylim([0, len(X) + (n_clusters + 1) * 10])
+
+        clusterer = KMeans(n_clusters=n_clusters, random_state=0)
+        cluster_labels = clusterer.fit_predict(X)
+
+        silhouette_avg = silhouette_score(X, cluster_labels)
+        print("For n_clusters =", n_clusters,
+            "The average silhouette_score is :", silhouette_avg)
+
+        sample_silhouette_values = silhouette_samples(X, cluster_labels)
+
+        y_lower = 10
+        for i in range(n_clusters):
+            ith_cluster_silhouette_values = \
+                sample_silhouette_values[cluster_labels == i]
+
+            ith_cluster_silhouette_values.sort()
+
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+
+            color = cm.nipy_spectral(float(i) / n_clusters)
+            ax1.fill_betweenx(np.arange(y_lower, y_upper),
+                            0, ith_cluster_silhouette_values,
+                            facecolor=color, edgecolor=color, alpha=0.7)
+
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+            y_lower = y_upper + 10
+
+        ax1.set_title("The silhouette plot for the various clusters.")
+        ax1.set_xlabel("The silhouette coefficient values")
+        ax1.set_ylabel("Cluster label")
+
+        ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+        ax1.set_yticks([])
+        ax1.set_xticks([-1, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    plt.show()
+
+run(1)
+run(2)
